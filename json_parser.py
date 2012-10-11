6a665f3bc0898107398c230001321d7139d81087
Likit Preeyanon <preeyano@msu.edu>
2012-10-11 12:21:13 -0400

    output is now written to output.csv in csv format.

--- json_parser.py
+++ json_parser.py
@@ -1,17 +1,19 @@
 '''Parse data in JSON format.'''
 
 import sys
+import csv
 import json
 
-for line in open(sys.argv[1]):
-    jsn = json.loads(line.strip())
-    print '%d\t%d\t%d\t%s\t%s\t%s\t%s\t%s' % \
-                                        (jsn['votes']['cool'],
-                                        jsn['votes']['funny'],
-                                        jsn['votes']['useful'],
-                                        jsn['user_id'],
-                                        jsn['name'],
-                                        jsn['url'],
-                                        jsn['average_stars'],
-                                        jsn['review_count'],
-                                    )
+with open('output.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for line in open(sys.argv[1]):
+        jsn = json.loads(line.strip())
+        writer.writerow([jsn['votes']['cool'],
+                            jsn['votes']['funny'],
+                            jsn['votes']['useful'],
+                            jsn['user_id'],
+                            jsn['name'],
+                            jsn['url'],
+                            jsn['average_stars'],
+                            jsn['review_count'],
+                        ])
