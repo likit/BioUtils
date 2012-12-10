40bbf7f4bb50388ccbd9fd9f172358d627f941ac
Likit Preeyanon <preeyano@msu.edu>
2012-12-10 00:11:53 -0500

    fixed all syntax errors.

--- size_select.py
+++ size_select.py
@@ -8,15 +8,15 @@ from Bio import SeqIO
 
 def main():
     try:
-        fastafile = sys.argv[1]
-        size = int(sys.argv[2]
+        fasta_file = sys.argv[1]
+        size = int(sys.argv[2])
     except IndexError:
         print >> sys.stderr, 'size_select.py <fasta file> <size>'
 
     for record in SeqIO.parse(fasta_file, 'fasta'):
         if len(record) >= size:
             print >> sys.stderr, 'Writing %s ..' % (record.id)
-            SeqIO.write(record, fastafile), sys.stdout, 'fasta')
+            SeqIO.write(record, fasta_file, sys.stdout, 'fasta')
 
 
 if __name__=='__main__':
