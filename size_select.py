c1a8f2cf662fc8e8c577484422ec63c2969962d9
Likit Preeyanon <preeyano@msu.edu>
2012-12-10 00:09:12 -0500

    added size_select.py

---
+++ size_select.py
@@ -0,0 +1,23 @@
+'''The script reads sequences in fasta format and selects sequences that are
+longer than a user specified length.
+
+'''
+
+import sys
+from Bio import SeqIO
+
+def main():
+    try:
+        fastafile = sys.argv[1]
+        size = int(sys.argv[2]
+    except IndexError:
+        print >> sys.stderr, 'size_select.py <fasta file> <size>'
+
+    for record in SeqIO.parse(fasta_file, 'fasta'):
+        if len(record) >= size:
+            print >> sys.stderr, 'Writing %s ..' % (record.id)
+            SeqIO.write(record, fastafile), sys.stdout, 'fasta')
+
+
+if __name__=='__main__':
+    main()
