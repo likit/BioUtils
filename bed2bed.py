cd161a787c5efd2a12061a759a9bbadfa1478cd0
Likit Preeyanon <preeyano@msu.edu>
2013-01-04 22:33:16 -0500

    added bed2bed.py

---
+++ bed2bed.py
@@ -0,0 +1,18 @@
+'''Convert gene models in BED format to BED format to be used as an
+input of multiBamCov to count reads map to each transcripts when reads are
+mapped directly to transcript sequences not genome sequence.
+Output is written to standard output.
+
+'''
+
+import sys
+import csv
+
+input_file = sys.argv[1]
+reader = csv.reader(open(input_file), dialect='excel-tab')
+for row in reader:
+    gene_id = row[3]
+    size = sum([int(s) for s in row[10].split(',')])
+    strand = row[5]
+    writer = csv.writer(sys.stdout, dialect='excel-tab')
+    writer.writerow([gene_id, 0, size, gene_id, 1000, strand])
